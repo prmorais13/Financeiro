f78d9b5a919405fda1886b65ec93ed471206e254
paulo <pauloroberto@natal.rn.gov.br>
2016-10-25 16:45:55 -0300

    segundo commit

--- Financeiro/src/main/java/br/com/financeiro/util/jpa/EntityManagerProducer.java
+++ Financeiro/src/main/java/br/com/financeiro/util/jpa/EntityManagerProducer.java
@@ -14,7 +14,7 @@ public class EntityManagerProducer {
 	private EntityManagerFactory factory;
 	
 	public EntityManagerProducer() {
-		factory = Persistence.createEntityManagerFactory("AgendaPU");
+		factory = Persistence.createEntityManagerFactory("FinanceiroPU");
 	}
 	
 	@Produces
